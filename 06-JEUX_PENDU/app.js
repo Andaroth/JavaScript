0e3f7e497d094433c0601aa32c23ad8d19f8a192
Anda <ax.fiolle@gmail.com>
2017-10-09 22:50:56 +0200

    update pendu

--- 06-JEUX_PENDU/app.js
+++ 06-JEUX_PENDU/app.js
@@ -1,5 +1,5 @@
-let soluce = ["B","O","N","J","O","U","R"];
-let found = ["","","","","","", ""];
+let soluce = ["L","O","V","E","L","A","C","E"];
+let found =  [" "," "," "," "," "," "," "," "];
 
 var limit = soluce.length;
 
@@ -15,52 +15,58 @@ var guessLetter = function() {
     console.log("you typed "+guess);
     
     if (guess.length != 1) {
-        console.log("Un seul caractère...");
+        console.log("Un seul charactère...");
+        guessLetter();
     }
     else if (alreadyTested.indexOf(guess) != -1) {
         console.log("Déjà entré :/");
-    }
-    
-    alreadyTested.push(guess);
-    tryNumb++;
-    
-    
-    
-    for (i=0;i<soluce.length;i++) {
-        
-        // Si il trouve !
-        if (guess == soluce[i])
-        {
-            console.log("catch "+guess+" at "+i);
-            
-            
-            found.splice(i,1,guess);
-            limit--;
-            
-            console.log(found[i]+" est ajouté à found à l'index "+i+" (il reste "+limit+" lettres à trouver)");
-
-        }
-        
-    }
-    
-    if (limit != 0) {
         guessLetter();
     }
     else {
-        console.log("FOUND == SOLUCE ! END now !");
-        document.write("<br/>&gt; Vous avez réussi en "+tryNumb+" essais. Le mot était ");
         
+        alreadyTested.push(guess);
+        tryNumb++;
+
+
+
         for (i=0;i<soluce.length;i++) {
-            document.write(found[i]);
-        }
-        document.write(" et vous vous êtes trompé <strong>"+(tryNumb-(found.length)+1)+"</strong> fois. ");
-        
-        if (alreadyTested.length > 0) {
-            document.write("<br/><br/>Vous avez essayé :<ul>")
-            for (i=0;i<alreadyTested.length;i++) {
-                document.write("<li>"+alreadyTested[i]+"</li>");
+
+
+            // Si il trouve !
+            if (guess == soluce[i])
+            {
+                console.log("catch "+guess+" at "+i);
+
+
+                found.splice(i,1,guess);
+                limit--;
+
+                console.log(found[i]+" est ajouté à found à l'index "+i+" (il reste "+limit+" lettres à trouver)");
+
             }
-            document.write("</ul>")
+
+        }
+
+        if (limit > 0) {
+            guessLetter();
+        }
+        else {
+            console.log("FOUND == SOLUCE ! END now !");
+            document.write("<br/>&gt; Vous avez réussi en "+tryNumb+" essais. Le mot était ");
+
+            for (i=0;i<soluce.length;i++) {
+                document.write(found[i]);
+            }
+            document.write(" et vous vous êtes trompé <strong>"+(tryNumb-(found.length)+2)+"</strong> fois. ");
+
+            if (alreadyTested.length > 0) {
+                document.write("<br/><br/>Vous avez essayé :<ul>")
+                for (i=0;i<alreadyTested.length;i++) {
+                    document.write("<li>"+alreadyTested[i]+"</li>");
+                }
+                document.write("</ul>")
+            }
+
         }
         
     }
