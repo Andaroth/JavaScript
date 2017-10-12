58ef3ab783113606f1268e0a41ef69018ad2385c
Anda <ax.fiolle@gmail.com>
2017-10-12 16:02:41 +0200

    Bonus ex8

--- 08-OBJETS/app.js
+++ 08-OBJETS/app.js
@@ -1,12 +1,20 @@
 let Character = {
     "name":"Bertrand",
-    "age":19,
-    "items_to_give":["MacBook","Thermos","Baffle","Enseignement","Aide"],
+    "level":19,
+    "life":100,
+    "items_to_give":["MacBook","Thermos","Baffle","Enseignement","Aide"]
 }
 
+let opponentCharacter= {};
 for (let i in Character) {
-    console.log(i+": "+Character[i]);
-    document.write("<br/>"+i+": "+Character[i]);
+    opponentCharacter[i] = Character[i];
+}
+opponentCharacter.name = "Axel";
+opponentCharacter.level = 24;
+
+let weapon = {
+    "name":"Épée",
+    "damage":1.3
 }
 
 function giveItem() {
@@ -17,4 +25,14 @@ function giveItem() {
     document.write("<br/>"+Character.name+" vous donne son "+Character.items_to_give[i]);
 }
 
-giveItem();
+function attack(a,b) {
+    let source = a;
+    let cible = b;
+    var degats = source["level"] * weapon["damage"];
+    document.write(source["name"]+" attaque "+cible["name"]+" et lui inflige "+degats+" de dommage<br/>");
+    cible["life"] -= degats;
+    document.write(cible["name"]+" a "+cible["life"]+" points de vie");
+}
+
+// giveItem();
+attack(Character,opponentCharacter);
