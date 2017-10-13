b68ff6068c0b4bf1ff7f9f18339379a1409d5e31
Anda <ax.fiolle@gmail.com>
2017-10-13 09:57:34 +0200

    1.0

--- 06-JEUX_PENDU/app.js
+++ 06-JEUX_PENDU/app.js
@@ -2,7 +2,7 @@
 let soluce = ["L","O","V","E","L","A","C","E"]; // Le mot qui valait trois milliards
 let found =  ["_","_","_","_","_","_","_","_"]; // un tableau (found) de la même longueur pour placer les lettres qu'on a trouvées
 var motMystere = soluce.join(""); // On enregistre le mot recherché pour le sortir à la fin en mode suspens
-var promptMot = found.join(); // Le mot pour afficher le mot caché dans le prompt
+var promptMot = found.join(" "); // Le mot pour afficher le mot caché dans le prompt
 var alertPrompt = ""; // Une chaîne pour indiquer l'erreur du joueur dans le prompt
 var motLong = soluce.length; // La longueur du mot
 var winCount = 0; // Le compteur de réussites
@@ -12,20 +12,39 @@ var leftTry = motLong - failNumb; // Le nombre d'essais restants
 let alreadyTested = []; // je créé un tableau pour enregistrer les essais du joueur
 // Je créé à l'avance la fonction gameOver.
 function gameOver() {
-    alert("Vous avez perdu... le pendu est PENDU !!! tin tiin~");
+    var lostMsg = "Vous avez perdu... le pendu est PENDU !!! tin tiin~";
+    // alert(lostMsg);
+    document.getElementById("alert").innerHTML = lostMsg;
 }
 // Je créé à l'avance la fonction gameWin.
 function gameWin() {
     console.log("Win !");
-    alert("Félicitations !! \nVous avez réussi en "+tryNumb+" essai(s). Le mot était donc "+motMystere+" et vous vous êtes trompé "+failNumb+" fois.");
+    var winMsg = "Félicitations !! \nVous avez réussi en "+tryNumb+" essai(s). Le mot était donc "+motMystere+" et vous vous êtes trompé "+failNumb+" fois.";
+    // alert(winMsg);
+    document.getElementById("alert").innerHTML = winMsg;
 }
+function addInTable(arg) {
+    var parent = document.getElementById("try_list");
+    var newLi= document.createElement("li");
+    newLi.innerHTML = "<li>"+arg+"</li>";
+    parent.appendChild(newLi);
+}
+
+function initialize() {
+    document.getElementById("try_show").innerHTML = promptMot;
+    document.getElementById("prompt").setAttribute( "autocomplete", "off" );
+}
+
 // La fonction principale
 function guessLetter() {
     if (leftTry == 0) { // On vérifie d'abord si le joueur n'a pas cramé tous ses essais
         gameOver(); // Sinon c'est bye bye, ciao, à la prochaine, merci d'être passé
     }
-    var guess = window.prompt(alertPrompt+"\nEssayez de trouver les lettres du pendu : \n"+promptMot,""); // Je demande la saisie au joueur
+     // var guess = window.prompt(alertPrompt+"\nEssayez de trouver les lettres du pendu : \n"+promptMot,""); // Je demande la saisie au joueur
+    var guess = document.getElementById("prompt").value;
+    document.getElementById('prompt').value = ""; 
     guess = guess.toUpperCase(); // Je dois mettre la lettre en MAJUSCULE pour qu'elle corresponde exactement à l'entrée du tableau
+    addInTable(guess);
     console.log("< you typed "+guess);
     /* 
         Je dois créer une boucle qui va tester la saisie.
@@ -33,21 +52,26 @@ function guessLetter() {
     */
     if (guess == motMystere) { // Si le joueur entre direct le mot recherché
         tryNumb++; // On ajoute un essai au compteur, car ça compte quand-même. 
+        promptMot = soluce.join(" ");
+        document.getElementById("try_show").innerHTML = promptMot; 
+        document.getElementById("alert").innerHTML = alertPrompt;
         console.log("GUESS RIGHT !!!");
         gameWin(); // Et c'est gagné :)
     }
     else if (guess.length != 1) { // Si l'utilisateur n'a rien rempli ou si il a mis plusieurs caractères
         alertPrompt = "! Mauvaise saisie, recommencez"; // Je définis un message d'erreur
+        document.getElementById("alert").innerHTML = alertPrompt;
         console.log(alertPrompt);
-        guessLetter(); // On repart au début de la fonction
+        // guessLetter(); // On repart au début de la fonction
     }
     else if (alreadyTested.indexOf(guess) != -1) { // Si la lettre a déjà été proposée
         tryNumb++; // On ajoute un essai au compteur car...
         failNumb++; // ...ça compte aussi comme un fail, vu que le joueur a les essais sous les yeux
         leftTry = motLong - failNumb; // refresh
         alertPrompt = "X Déjà essayé, "+leftTry+" essais restants";
+        document.getElementById("alert").innerHTML = alertPrompt;
         console.log(alertPrompt);
-        guessLetter(); // On repart au début de la fonction
+        // guessLetter(); // On repart au début de la fonction
     }
     else { // Si aucune exception n'est rencontrée, c'est parti pour le début de la fin, une grande boucle va tourner
         alreadyTested.push(guess); // Premièrement, j'ajoute la saisie au tableau des... saisies :) Et ce à chaque tour donc. 
@@ -56,9 +80,10 @@ function guessLetter() {
             failNumb++; // Si la saisie est fausse, non seulement j'te colle un fail... 
             leftTry = motLong - failNumb; // refresh
             alertPrompt = "X Nope, "+leftTry+" essais restants";
+            document.getElementById("alert").innerHTML = alertPrompt;
             console.log(alertPrompt);
             
-            guessLetter(); // ... mais en plus tu retourne au début!
+            // guessLetter(); // ... mais en plus tu retourne au début!
         } 
         console.log("passed"); // Du coup, seuls les résultats positifs passent à la suite du code
         
@@ -75,7 +100,8 @@ function guessLetter() {
 
                 found.splice(i,1,guess); // On fait entrer la lettre saisie (guess) dans le tableau found à l'emplacement i. Càd à sa place dans l'ordre du mot. 
                 winCount++; // On indique qu'une lettre à été trouvée
-                promptMot = found.join(); // On actualise l'aperçu du mot dans le prompt
+                promptMot = found.join(" "); 
+                document.getElementById("try_show").innerHTML = promptMot; // On actualise l'aperçu du mot dans le prompt
                 console.log(">>"+found[i]+" est ajouté à found à l'index "+i+" ("+winCount+" lettres ont été trouvées)");
             }
         } // la raison cette boucle un peu spéciale est de trouver toutes les lettres identiques en seul tour car telles sont les règles du pendu
@@ -84,13 +110,11 @@ function guessLetter() {
             Une fois que la boucle a fini de routourner, 
             On vérifie si le joueur a trouvé toutes les lettres en comparant le compteur de lettres trouvées avec la longueur du mot. 
         */
-        if (winCount != motLong) { // Si le compte n'y est pas, on retourne directement dans la boucle !
-            guessLetter();
-        }
-        else { // Sinon félicitations, bravo, congratulations ! Les esclaves peuvent arrêter de tourner, le jeu est terminé tu as gagné !
+        if (winCount == motLong) { // Si le compte y est, félicitations, bravo, congratulations ! Les esclaves peuvent arrêter de tourner, le jeu est terminé tu as gagné !
             gameWin(); // Go fonction win :)
         }  
     } // La fin du grand else des exceptions
 } // La fin de la déclaration guessLetter();
 // Faut pas oublier de faire tourner la routourne en lançant la fonction principale ! :D 
-guessLetter();
+initialize();
+document.getElementById("send").onclick = function(){guessLetter()};
--- 08-OBJETS/app.js
+++ 08-OBJETS/app.js
@@ -1,7 +1,10 @@
 let Character = {
     "name":"Bertrand",
     "level":19,
-    "life":100,
+    "life":300,
+    "atk":12,
+    "def":13,
+    "agil":25,
     "items_to_give":["MacBook","Thermos","Baffle","Enseignement","Aide"]
 }
 
@@ -11,11 +14,20 @@ for (let i in Character) {
 }
 opponentCharacter.name = "Axel";
 opponentCharacter.level = 24;
+opponentCharacter.atk = 15;
+opponentCharacter.def = 13;
+opponentCharacter.agil = 21;
 
-let weapon = {
+let sword = {
     "name":"Épée",
     "damage":1.3
 }
+let gun = {}
+for (let i in sword) {
+    gun[i] = sword[i];
+}
+gun.name="Pistolet";
+gun.damage=2.6;
 
 function giveItem() {
     var i = Math.floor(Math.random() * Character.items_to_give.length);
@@ -25,12 +37,32 @@ function giveItem() {
     document.write("<br/>"+Character.name+" vous donne son "+Character.items_to_give[i]+"<br/>");
 }
 
-function attack(source,cible) {
-    var degats = source["level"] * weapon["damage"];
-    document.write(source["name"]+" attaque "+cible["name"]+" et lui inflige "+degats+" de dommage<br/>");
+function attack(source,cible,arme) {
+    var degats = 
+        Math.ceil((
+            source["level"] * 
+            (arme["damage"] * ((source["atk"])/10)) /
+            (
+                (cible["def"]) * (Math.random() * cible["agil"])
+            )
+        )*(100-cible["agil"]));
+    
+    if (degats > source["atk"]*arme["damage"])
+    {
+        document.write("<strong>Critique !!</strong><br/>");
+    }
+    
+    document.write(source["name"]+" attaque "+cible["name"]+" avec "+arme["name"]+" et lui inflige "+degats+" de dommage<br/>");
     cible["life"] -= degats;
-    document.write(cible["name"]+" a "+cible["life"]+" points de vie");
+    document.write(cible["name"]+" a "+cible["life"]+" points de vie<br/>");
+    
+    if (cible["life"] <= 0)
+    {
+        document.write("<strong>"+cible["name"]+" est mort...</strong><br/>");
+    }
+    
 }
 
 giveItem();
-attack(Character,opponentCharacter);
+attack(Character,opponentCharacter,gun);
+
